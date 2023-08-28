45c23b2ce109c0c93a05c96ed6159585dbc3e12b
leary423 <leary620@163.com>
2023-08-28 18:30:38 +0800

    add css

--- pages/community/index.vue
+++ pages/community/index.vue
@@ -26,18 +26,18 @@
   <div class="bg-[#000000] relative">
     <div class="community-bg-cost2">
     <img src="~/assets/images/community-bg-cost.png" class="h-full w-full absolute" />
-      <div class="container mx-auto px-5 pb-[120px]">
-        <div class="py-[100px] text-center flex justify-center">
-          <div class="text-[48px] md:w-[700px] font-extrabold font-family-extraBold">Build your team quickly, worry-free and at low cost</div>
+      <div class="container mx-auto px-5 pb-[100px] md:pb-[120px]">
+        <div class="pt-[60px] pb-[50px] md:py-[100px] text-center flex justify-center">
+          <div class="text-[24px] md:text-[48px] w-[260px] md:w-[700px] font-extrabold font-family-extraBold">Build your team quickly, worry-free and at low cost</div>
         </div>
-        <div class="grid grid-cols-2 gap-6">
+        <div class="grid md:grid-cols-2 gap-6">
           <div v-for="(item,key) in buildTeamList" :key="key" class="card-box-border">
-            <div class="bg-[#000000] p-[50px] h-full rounded-[20px]">
-              <div :class="[key==0?'card-text-css':'card-text-css2']" class="flex text-[32px] font-bold">{{ item.title }}</div>
-              <div class="mt-[30px] mb-[40px] text-[18px] font-light font-family-Light leading-[20px]">{{ item.desc }}</div>
-              <div class="community-border-t pt-[40px]">
-                <div v-for="(subItem,subKey) in item.subList" :key="subKey" class="mb-[30px] flex">
-                  <img src="~/assets/images/ecosystem-sub.png" class="h-[20px] w-[20px] mr-[10px]"/>
+            <div class="bg-[#000000] p-[30px] md:p-[50px] h-full rounded-[20px]">
+              <div :class="[key==0?'card-text-css':'card-text-css2']" class="flex text-[21px] md:text-[32px] font-bold">{{ item.title }}</div>
+              <div class="mt-[20px] md:mt-[30px] mb-[30px] md:mb-[40px] text-[12px] md:text-[18px] font-light font-family-Light leading-[13px] md:leading-[20px]">{{ item.desc }}</div>
+              <div class="community-border-t pt-[20px] md:pt-[40px] text-[12px] md:text-[16px]">
+                <div v-for="(subItem,subKey) in item.subList" :key="subKey" class="mb-[20px] md:mb-[30px] flex leading-[15px] md:leading-[20px]">
+                  <img src="~/assets/images/ecosystem-sub.png" class="h-[14px] w-[14px] md:h-[20px] md:w-[20px] mr-[10px]"/>
                   <div class="text-[#C5C5C5]" v-html="subItem.content"></div>
                 </div>
               </div>
