6bcdb91a3b6aa2797bf397f52e475e5bd2480b7b
wangshuqin <shuqin.wang@newtouch.com>
2023-12-27 15:29:30 +0800

    test

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <a-Button>按钮</a-Button>
+  <div class="test mt-[100px] mb-[100px]">
+    <div class="text-[20px] txte-[#000000]">我要测试ant-design-vue为啥用不了</div>
+    <Button type="primary" class="mr-[20px]">Primary Button</Button>
+    <Button class="mr-[20px]">Default Button</Button>
+    <Button type="dashed" class="mr-[20px]">Dashed Button</Button>
+    <Button type="text" class="mr-[20px]">Text Button</Button>
+    <Button type="link">Link Button</Button>
   </div>
 </template>
 <script lang='ts' setup>
+import { Button } from "ant-design-vue"
 </script>
 <style lang='less' scoped></style>
